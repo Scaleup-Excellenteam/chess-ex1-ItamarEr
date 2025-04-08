f2b0b8adfffbd43189ab466cd161b60ea8b0c4fb
Itamar Erez <itamarrerez@gmail.com>
2025-04-08 15:41:07 +0300

    added Pawn class

--- Chess/include/Pawn.h
+++ Chess/include/Pawn.h
@@ -5,12 +5,17 @@
 #ifndef PAWN_H
 #define PAWN_H
 
+#include "Piece.h"
 
-
-class Pawn {
+/**
+ * Pawn class representing a pawn piece in chess.
+ */
+class Pawn : public Piece {
+public:
+    Pawn(int row, int col, bool is_white);
+    bool isValidMove(int target_row, int target_col) override;
 
 };
-
 
 
 #endif //PAWN_H
