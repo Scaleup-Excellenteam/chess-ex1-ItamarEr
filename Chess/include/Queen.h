59055bcd5386ba48b4aa991939bfa983c9834d9e
Itamar Erez <itamarrerez@gmail.com>
2025-04-08 15:39:18 +0300

    added Queen class

--- Chess/include/Queen.h
+++ Chess/include/Queen.h
@@ -5,12 +5,16 @@
 #ifndef QUEEN_H
 #define QUEEN_H
 
+#include "Piece.h"
 
-
-class Queen {
-
+/**
+ * Queen class representing a queen piece in chess.
+ */
+class Queen : public Piece {
+public:
+    Queen(int row, int col, bool is_white);
+    bool isValidMove(int target_row, int target_col) override;
 };
-
 
 
 #endif //QUEEN_H
