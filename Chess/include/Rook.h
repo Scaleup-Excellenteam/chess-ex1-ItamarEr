bc24caf86577fd2a39b4a2d6ad14160906dc5d46
Itamar Erez <itamarrerez@gmail.com>
2025-04-08 15:38:51 +0300

    added Rook class

--- Chess/include/Rook.h
+++ Chess/include/Rook.h
@@ -5,9 +5,15 @@
 #ifndef ROOK_H
 #define ROOK_H
 
+#include "Piece.h"
 
-
-class Rook {
+/**
+ * Rook class representing a rook piece in chess.
+ */
+class Rook: public Piece {
+public:
+    Rook(int row, int col, bool is_white);
+    bool isValidMove(int target_row, int target_col) override;
 
 };
 
